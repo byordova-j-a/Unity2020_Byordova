2a4946280f4c2d69d13062a3c996fdcc4afcadd6
byordova-j-a <berd.rabbit@gmail.com>
2020-11-16 21:42:59 +0500

    Init project commit
    доделанные и исправленные задания с пружинным ( colebabia) и математическим (matem_mayatn) маятниками. (случайно удалила первое задание вместе с неполучившимися до этого заданиями, поэтому сейчас и его добавила обратно)

---
+++ Rollaball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0;
+
+    private Rigidbody rb;
+public TextMeshProUGUI countText;
+	public GameObject winTextObject;
+    private float movementX;
+    private float movementY;
+    private int count;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+         count = 0;
+
+		SetCountText ();
+winTextObject.SetActive(false);
+    }
+
+    private void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp")) 
+        {
+            other.gameObject.SetActive(false);
+                 count = count + 1;
+
+			// Run the 'SetCountText()' function (see below)
+			SetCountText ();
+        }
+    }
+void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString();
+
+		if (count >= 12) 
+		{
+                    // Set the text value of your 'winText'
+                    winTextObject.SetActive(true);
+		}
+	}
+}
