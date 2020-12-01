78a5bbf31cbdeeb19549e18099724df762a86148
justjune <justjune@live.ru>
2020-12-01 19:15:53 +0500

    20201213 some additional corrections

--- moving_central_force_unity/moving_central_force_unity/Assets/Scripts/moving.cs
+++ moving_central_force_unity/moving_central_force_unity/Assets/Scripts/moving.cs
@@ -5,69 +5,86 @@ using UnityEngine;
 public class moving : MonoBehaviour
 {
     // Start is called before the first frame update
-	public GameObject obj;
+//	public GameObject obj;
 	private Rigidbody rb;
-	private float c=0;
-	public float z=0;
+    private float c = 0f;
+	public int z=0;
 	private Vector3 start_impulse;
 	private float distanse=0;
 	private Vector3 cos;
 	private float alfa;
+    enum Trj
+    { prb = 1, elp, gpr, crc }
     void Start()
-    { rb=GetComponent<Rigidbody>();
-	distanse=transform.position.magnitude;
-		if (z==1f)
-		{
-		start_impulse=Mathf.Sqrt(2*distanse*9.8f)*new Vector3(-Mathf.Sqrt(2)/2,0,Mathf.Sqrt(2)/2);
-       
-		rb.AddForce(start_impulse, ForceMode.VelocityChange);
-		}
-		if (z==2f)
-		{
-			alfa= Mathf.PI/2-Mathf.Atan(1/2);
-			start_impulse=Mathf.Sqrt(distanse*9.8f)*new Vector3(-Mathf.Cos(alfa),0,Mathf.Sin(alfa));
-       
-		rb.AddForce(start_impulse, ForceMode.VelocityChange);
-			
-		}
-		if (z==3)
-		{
-			alfa= Mathf.PI/2-Mathf.Atan(7/4);
-			start_impulse=Mathf.Sqrt(6f*distanse*9.8f)*new Vector3(-Mathf.Cos(alfa),0,Mathf.Sin(alfa));
-       
-		rb.AddForce(start_impulse, ForceMode.VelocityChange);
-			
-		}
-		if (z==4)
-		{
-			alfa= Mathf.PI/2;
-			start_impulse=Mathf.Sqrt(1.5f*distanse*9.8f)*new Vector3(0,0,1);
-       
-		rb.AddForce(start_impulse, ForceMode.VelocityChange);
-			
-		}
-		c= distanse*distanse*9.8f;
-    }
+    { 
+		rb=GetComponent<Rigidbody>();
+		distanse=transform.position.magnitude;
+		float c0;
+		c0 = Mathf.Sqrt(2f * distanse * 9.8f);
 
+		switch ((Trj)z) {
+			case Trj.prb:
+			//start_impulse=c0*new Vector3(-transform.position.y/distanse,0,transform.position.x/distanse);
+			start_impulse = c0 * new Vector3(-transform.position.y / distanse, 0, transform.position.x / distanse);
+			//rb.velocity= start_impulse;
+			rb.AddForce(start_impulse, ForceMode.Impulse);
+				break;
+			case Trj.elp:
+			alfa= Mathf.PI/2-Mathf.Atan(1/2);
+			start_impulse=c0*new Vector3(-Mathf.Cos(alfa),0,Mathf.Sin(alfa));
+			//rb.velocity = start_impulse;
+			rb.AddForce(start_impulse, ForceMode.Impulse);
+				break;
+			case Trj.gpr:
+			alfa= Mathf.PI/2-Mathf.Atan(7/4);
+			start_impulse=c0*new Vector3(-Mathf.Cos(alfa),0,Mathf.Sin(alfa));
+			//rb.velocity = start_impulse;
+			rb.AddForce(start_impulse, ForceMode.Impulse);
+				break;
+
+			case Trj.crc:
+			alfa= Mathf.PI/2;
+			//start_impulse=c0*new Vector3(0,0,1);
+			start_impulse = c0 * new Vector3(-transform.position.y / distanse, 0, transform.position.x / distanse);
+			rb.AddForce(start_impulse, ForceMode.Impulse);
+				break;			
+		}
+		print(z.ToString() + ": " + start_impulse.magnitude.ToString());
+		c= distanse*distanse*9.8f;
+		calc();
+    }
+	void calc()
+    {
+		distanse = transform.position.magnitude;
+
+
+
+		if (distanse < 0.5f)
+		{
+			Vector3 nul = new Vector3(0, 0, 0);
+			//transform.position=nul;
+			rb.AddForce(nul, ForceMode.Impulse);
+			rb.isKinematic = true;
+			rb.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionZ;
+			Debug.Log("Full Stop!");
+			Debug.Log(z.ToString() + ": " + transform.position.x.ToString() + " " + transform.position.y.ToString() + " " + transform.position.z.ToString());
+			enabled = false;
+		}
+		else
+		{
+			Vector3 prit = -transform.position * c / (distanse * distanse * distanse);
+			if (z == 4)
+			{
+				//Debug.Log(prit.magnitude);
+			}
+			rb.AddForce(prit, ForceMode.Force);
+		}
+	}
     // Update is called once per frame
     void FixedUpdate()
-    {   
-        distanse=transform.position.magnitude;
-		
-		
-		
-		if (distanse < 0.15)
-		{
-			Vector3 nul = new Vector3(0,0,0);
-			transform.position=nul;
-			//rb.AddForce(nul,ForceMode.Acceleration);
-			rb.isKinematic=true;
-			
-		} else{
-		
-		rb.AddForce(-transform.position*c/(distanse*distanse*distanse), ForceMode.Impulse);
-		}
-		
+    {
+
+		calc();
 		//Debug.Log(distanse);
     }
 }
